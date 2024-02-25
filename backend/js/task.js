58cfffd151d23fe34b115d5fabdcd7005f7d5cc1
Hellen Wang <hellenyilin23@gmail.com>
2024-02-26 00:19:54 +1100

    finished tasks 2 and 3 and testing

--- backend/js/task.js
+++ backend/js/task.js
@@ -3,24 +3,18 @@
  * Task 1
  */
 function leafFiles(files) {
-    const parents = [];
+
     const leafFiles = [];
 
-    for (const file of files) {
-        parents.push(file.parent);
-    }
+    const parents = files.map(file => file.parent)
 
     for (const file of files) {
-        const parent = parents.find(a => a == file.id);
+        const parent = parents.find(a => a === file.id);
         
         if (!parent) {
             leafFiles.push(file.name)
         }
     }
-
-    // const leafFiles = files.filter(file => !files.some(file => file.parent === file.id))
-
-    // console.log(leafFiles) 
 
     return leafFiles;
 }
@@ -30,18 +24,74 @@ function leafFiles(files) {
  */
 function kLargestCategories(files, k) {
 
-    // go through the files and find the k most occured catagories - and find the relative sizes
-    // sort them by size / alphabetical order .sort()
+    // go through the files and find the k most occured catagories
+    // sort them by count / alphabetical order .sort()
 
-    return [];
+    const categories = [];
+
+    for (const file of files) {
+        for (const category of file.categories) {
+            
+            const existingCategory = categories.find(a => a.name === category)
+    
+            if(!existingCategory) {
+                categories.push({
+                    name: category,
+                    count: 1,
+                })
+            } else {
+                existingCategory.count++;
+
+            }
+        }
+    }
+
+    categories.sort((category1, category2) => {
+        if (category1.count !== category2.count) {
+            return category2.count - category1.count;
+        } else {
+            return category1.name.localeCompare(category2.name);
+        }
+    });
+
+    categories.splice(k,);
+
+    return categories.map(category => category.name);
 }
 
 /**
  * Task 1
  */
 function largestFileSize(files) {
-    return 0;
+
+    const parents = files.filter(file => file.parent === -1)
+
+    let max = 0;
+    for (const parent of parents) {
+        const size = largestFileRecur(parent, files) + parent.size
+        
+        if (size > max) {
+            max = size;
+        }
+    }
+
+    return max;
 }
+
+function largestFileRecur(parent, files) {
+    const children = files.filter(file => file.parent === parent.id)
+
+    let totalSize = 0;
+
+    for (const child of children) {
+        totalSize += child.size;
+        totalSize += largestFileRecur(child, files);
+    }
+
+    return totalSize
+}
+
+
 
 
 function arraysEqual(a, b) {
@@ -70,6 +120,11 @@ const testFiles = [
     { id: 233, name: "Folder3", categories: ["Folder"], parent: -1, size: 4096 },
 ];
 
+const testFiles2 = [
+    { id: 1, name: "Document.txt", categories: ["Documents"], parent: 3, size: 1024 },
+    { id: 2, name: "Image.jpg", categories: ["Media", "Photos"], parent: 34, size: 2048 },
+];
+
 console.assert(arraysEqual(
     leafFiles(testFiles).sort((a, b) => a.localeCompare(b)),
     [
@@ -89,5 +144,11 @@ console.assert(arraysEqual(
     kLargestCategories(testFiles, 3),
     ["Documents", "Folder", "Media"]
 ));
+
+console.assert(arraysEqual(
+    kLargestCategories(testFiles2, 20),
+    ["Documents", "Media", "Photos"]
+));
+
 
 console.assert(largestFileSize(testFiles) == 20992)
