603f56996c04c323b3f7d9f6cd74a19dc938ba8a
Hellen Wang <hellenyilin23@gmail.com>
2024-02-27 18:23:14 +1100

    attempted q1

--- chaos/src/data.rs
+++ chaos/src/data.rs
@@ -4,8 +4,24 @@ use serde::{Deserialize, Serialize};
 pub async fn process_data(Json(request): Json<DataRequest>) -> impl IntoResponse {
     // Calculate sums and return response
 
+    let data = request.data;
+
+    let mut string_len = 0;
+    let mut int_sum = 0;
+
+    string_len += data[0].len();
+    int_sum += data[1].parse::<i32>().unwrap();
+    int_sum += data[2].parse::<i32>().unwrap();
+    string_len += data[4].len();
+    string_len += data[5].len();
+
+    println!("{}", string_len);
+    println!("{}", int_sum);
+    
+
     let response = DataResponse {
-        
+        string_len: string_len,
+        int_sum: int_sum,
     };
 
     (StatusCode::OK, Json(response))
@@ -14,9 +30,12 @@ pub async fn process_data(Json(request): Json<DataRequest>) -> impl IntoResponse
 #[derive(Deserialize)]
 pub struct DataRequest {
     // Add any fields here
+    data: Vec<String>
 }
 
 #[derive(Serialize)]
 pub struct DataResponse {
     // Add any fields here
+    string_len: i32,
+    int_sum: i32,
 }
